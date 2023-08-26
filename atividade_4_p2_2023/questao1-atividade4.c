b9120ff958dccf2e6a1dc41d7115733923afbd13
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2023-08-26 15:03:35 +0000

    Create questao1-atividade4.c

---
+++ atividade_4_p2_2023/questao1-atividade4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    printf("Digite um número inteiro:\n");
+    scanf("%d", &number);
+
+    if (number > 10) {
+        printf("O valor é maior que 10\n");
+    }
+    else if (number = 10)
+    {
+        printf("O valor é maior que 10\n");
+    }
+    else
+    {
+        printf("O valor é maior que 10\n");
+    }
+    
+        
+    return 0;
+}
