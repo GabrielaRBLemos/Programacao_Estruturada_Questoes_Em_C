d87704a2991b199549e13fa1953fde0845630f61
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2023-08-26 14:52:52 +0000

    Create questao1-atividade3.c
    Implemente o código do slide de número 23.

---
+++ questao1-atividade3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x;
+    float y;
+    x = 10;
+    y = 5.0;
+    printf("%d\n", x);
+    printf("%d%f\n", x, y);
+    printf("%d %f\n", x, y);
+    return 0;
+}
+
